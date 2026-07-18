e050ecb880cbd43fe9565846f2f2643de6c7882e
Sanna Kramsi <sanna.kramsi@gmail.com>
2026-07-18 18:07:49 +0300

    feat(Security): Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
 import icon from "astro-icon";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.sanna.ninja',
-  integrations: [sitemap(), react(), icon()],
+  integrations: [sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
